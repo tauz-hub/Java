e2d31e810904b9d0185ea630b509678c290b232f
Tauã Santo <86664818+tauzxd@users.noreply.github.com>
2021-07-16 12:31:37 -0300

    Java

---
+++ calculate pi/src/calcularPI/calculadora_pi.java
@@ -0,0 +1,22 @@
+package calcularPI;
+import java.util.Scanner;
+
+public class calculadora_pi {
+
+
+	public static void main(String[] args) {
+		String setResult="";
+		int points;
+		
+		Scanner tcd = new Scanner(System.in);
+		System.out.println("pontos");
+		points = tcd.nextInt();
+		
+		
+		System.out.println(setResult);
+	}
+	public static String setResult(int points) 
+	{ return "OK"; }
+
+}
+
