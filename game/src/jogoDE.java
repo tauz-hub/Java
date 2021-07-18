242cc3c1969621442ea4ae68beda7d59464a6813
Tauã Santo <86664818+tauzxd@users.noreply.github.com>
2021-07-17 23:26:41 -0300

    Java

---
+++ game/src/jogoDE.java
@@ -0,0 +1,10 @@
+
+public class jogoDE {
+	public static void main(String[] args) {
+		String setResult="";
+		System.out.println(setResult);
+	}
+	public static String setResult(int points) 
+	{ return "OK"; }
+
+}
